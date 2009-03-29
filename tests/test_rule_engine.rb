cb8b40d20d2936f276d70bba62c5875c66df8baf
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-03-29 21:30:31 +0900

    Synchronization free Rule/Action (mainly) restructuring. Dependent eventmachine was upgraded and the compatible version is 0.12.6.

---
+++ tests/test_rule_engine.rb
@@ -0,0 +1,14 @@
+
+
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require 'rubygems'
+
+require 'test/unit'
+require 'wakame'
+require 'wakame/rule.rb'
+
+WAKAME_ROOT="#{File.dirname(__FILE__)}/.."
+
+class TestRuleEngine < Test::Unit::TestCase
+  def test_
+end
