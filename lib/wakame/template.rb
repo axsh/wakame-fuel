6e621e3f7699821e974f3bcf41daf70d32477200
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-05-30 22:32:20 +0900

    Renamed the configration template processing class.

--- lib/wakame/template.rb
+++ lib/wakame/template.rb
@@ -1,8 +1,84 @@
 
 require 'fileutils'
+require 'erb'
 
 module Wakame
-  module ConfigurationTemplate
+  class Template
+    attr_accessor :service_instance
+    attr_reader :tmp_basedir
+
+    def initialize(service_instance)
+      @service_instance = service_instance
+      @tmp_basedir = File.expand_path(Util.gen_id, File.join(Wakame.config.root_path, 'tmp', 'config') )
+      FileUtils.mkdir_p @tmp_basedir
+    end
+
+    def basedir
+      @service_instance.resource.basedir
+    end
+
+    def render_config
+      service_instance.property.render_config(self)
+    end
+
+    def cleanup
+      FileUtils.rm_r( @tmp_basedir, :force=>true)
+    end
+
+    def render(args)
+      args = [args] if args.is_a? String
+      
+      args.each { |path|
+        update(path) { |buf|
+          ERB.new(buf, nil, '-').result(service_instance.export_binding)
+        }
+      }
+    end
+
+    def cp(args)
+      args = [args] if args.is_a? String
+      args.each { |fname|
+
+        destpath = File.expand_path(fname, @tmp_basedir)
+        FileUtils.mkpath(File.dirname(destpath)) unless File.directory?(File.dirname(destpath))
+
+        FileUtils.cp_r(File.join(basedir, fname),
+                       destpath,
+                       {:preserve=>true}
+                       )
+      }
+    end
+
+    def chmod(fname, mode)
+      File.chmod(mode, File.join(@tmp_basedir, fname))
+    end
+
+    def load(path)
+      path = path.sub(/^\//, '')
+      
+      return File.readlines(File.expand_path(path, basedir), "r").join('')
+    end
+    
+    def save(path, buf)
+      path = path.sub(/^\//, '')
+      
+      destpath = File.expand_path(path, @tmp_basedir)
+      FileUtils.mkpath(File.dirname(destpath)) unless File.directory?(File.dirname(destpath))
+      
+      File.open(destpath, "w", 0644) { |f|
+        f.write(buf)
+      }
+    end
+    
+    def update(path, &blk)
+      buf = load(path)
+      buf = yield buf if block_given?
+      save(path, buf)
+    end
+    
+  end
+
+  module Template2
     class Base
       attr_reader :tmp_output_basedir
       
@@ -11,7 +87,7 @@ module Wakame
 
 
       def pre_render
-        @tmp_output_basedir = File.expand_path(Wakame.gen_id, Wakame.config.config_tmp_root)
+        @tmp_output_basedir = File.expand_path(Util.gen_id, Wakame.config.config_tmp_root)
         FileUtils.mkdir_p @tmp_output_basedir
       end
 
@@ -27,7 +103,6 @@ module Wakame
       end
 
 
-      protected
       def load(path)
         path = path.sub(/^\//, '')
 
