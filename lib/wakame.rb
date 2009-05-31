6f2dc6698697f10e4f6058d142286e6c563feb3d
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-05-31 21:26:24 +0900

    Removed require() from the file and added autoloads for new class files.

--- lib/wakame.rb
+++ lib/wakame.rb
@@ -1,17 +1,13 @@
 
 require 'rubygems'
 
-require 'eventmachine'
-require 'ext/eventmachine'
-require 'amqp'
-require 'mq'
-
 # For debugging
 Thread.abort_on_exception = true 
 
 module Wakame
   VERSION='0.3.1'
   autoload :Agent, 'wakame/agent'
+  autoload :Master, 'wakame/master'
   autoload :Daemonize, 'wakame/daemonize'
   autoload :Util, 'wakame/util'
   autoload :Event, 'wakame/event'
@@ -23,6 +19,13 @@ module Wakame
   autoload :Configuration, 'wakame/configuration'
   autoload :Logger, 'wakame/logger'
   autoload :Packets, 'wakame/packets'
+  autoload :AMQPClient, 'wakame/amqp_client'
+  autoload :EventDispatcher, 'wakame/event_dispatcher'
+  autoload :Scheduler, 'wakame/scheduler'
+  autoload :Command, 'wakame/command'
+  autoload :CommandQueue, 'wakame/command_queue'
+  autoload :Template, 'wakame/template'
+  autoload :Trigger, 'wakame/trigger'
   #autoload :Initializer, 'wakame/initializer' # Do not autoload this class since the constant is used for the flag in bootstrap.
 
   def gen_id(str=nil)
