d4df4f60a6216f5c16671fae1d682fcf02b2784a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-05-31 21:27:17 +0900

    Added the status constants and map_path attribute.

--- lib/wakame/actor.rb
+++ lib/wakame/actor.rb
@@ -2,6 +2,11 @@
 
 module Wakame
   module Actor
+    STATUS_RUNNING = 1
+    STATUS_SUCCESS = 2
+    STATUS_FAILED = 0
+    STATUS_CANCELED = 3
+
     def self.included(klass)
       klass.extend ClassMethods
       klass.class_eval {
@@ -13,6 +18,14 @@ module Wakame
       def expose(path, meth)
         @exposed ||= {}
         @exposed[path]=meth
+      end
+
+      def map_path
+        @map_path ||= Util.snake_case(self.to_s.split('::').last)
+      end
+      
+      def map_path=(name)
+        @map_path=name
       end
     end
     
