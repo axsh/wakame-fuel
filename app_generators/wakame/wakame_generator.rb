2a4ce8da85b7ee91c49519f35dd3840624fcc73a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-06-01 16:06:27 +0900

    Added initial support for the project generator command and templates.

---
+++ app_generators/wakame/wakame_generator.rb
@@ -0,0 +1,152 @@
+require 'rbconfig'
+
+class WakameGenerator < RubiGen::Base
+
+  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
+                              Config::CONFIG['ruby_install_name'])
+
+  default_options   :shebang => DEFAULT_SHEBANG,
+  :bin_name    => nil,
+  :import_path => nil,
+  :test_framework  => 'test_unit',
+  :version     => '0.0.1'
+
+
+  attr_reader :gem_name, :module_name, :project_name
+  attr_reader :version, :version_str
+
+  # extensions/option
+  attr_reader :test_framework
+  attr_reader :bin_names_list
+  attr_reader :enable_website
+  attr_reader :manifest
+
+  def initialize(runtime_args, runtime_options = {})
+    super(config_args_and_runtime_args(runtime_args), runtime_options)
+    usage if args.empty?
+    @destination_root = File.expand_path(args.shift)
+    @gem_name = base_name
+    @module_name  = gem_name.gsub('-','_').camelize
+    @project_name = @gem_name
+    extract_options
+  end
+
+  def manifest
+    # Use /usr/bin/env if no special shebang was specified
+    script_options = { :chmod => 0755, :shebang => options[:shebang] == DEFAULT_SHEBANG ? nil : options[:shebang] }
+    windows        = (RUBY_PLATFORM =~ /dos|win32|cygwin/i) || (RUBY_PLATFORM =~ /(:?mswin|mingw)/)
+
+    record do |m|
+      # Root directory and all subdirectories.
+      m.directory ''
+      BASEDIRS.each { |path| m.directory path }
+
+      m.file_copy_each %w(Rakefile README)
+      m.file_copy_each %w(config/boot.rb config/environments/common.rb config/environments/standalone.rb  config/environments/ec2.rb)
+      #m.template "lib/module.rb",         "lib/#{gem_name}.rb"
+
+      # Selecting a test framework
+      #case test_framework
+      #when "test_unit"
+      #  m.dependency "install_test_unit", [gem_name], :destination => destination_root, :collision => :force
+      #when "rspec"
+      #  m.dependency "install_rspec", [gem_name], :destination => destination_root, :collision => :force
+      #when "shoulda"
+      #  m.dependency "install_shoulda", [gem_name], :destination => destination_root, :collision => :force
+      #end
+
+      # Executables
+      for bin_name in bin_names_list
+        m.dependency "executable", [bin_name], :destination => destination_root, :collision => :force
+      end
+      
+      for generator in @install_generators
+        m.dependency "install_#{generator}", [], :destination => destination_root, :collision => :force
+      end
+      
+      #m.dependency "install_rubigen_scripts", [destination_root, "rubygems", "newgem", "newgem_theme"], :shebang => options[:shebang], :collision => :force
+
+      %w( console ).each do |file|
+        m.template "script/#{file}.erb",        "script/#{file}", script_options
+        m.template "script/win_script.cmd", "script/#{file}.cmd",
+        :assigns => { :filename => file } if windows
+      end
+
+      #m.write_manifest "Manifest.txt"
+
+      m.readme "readme"
+    end
+  end
+
+  protected
+  def banner
+    <<-EOS
+Take any library or Rails plugin or command line application,
+gemify it, and easily share it with the Ruby world.
+
+Usage: #{File.basename $0} /path/to/your/app [options]
+EOS
+  end
+
+  def add_options!(opts)
+    opts.separator ''
+    opts.separator 'Options:'
+    opts.on("-b", "--bin-name=BIN_NAME[,BIN_NAME2]", String,
+            "Sets up executable scripts in the bin folder.",
+            "Default: none") { |x| options[:bin_name] = x }
+    opts.on("-i", "--install=generator", String,
+            "Installs a generator called install_<generator>.",
+            "For example, '-i cucumber' runs the install_cucumber generator.",
+            "Can be used multiple times for different generators.",
+            "Cannot be used for generators that require argumnts.",
+            "Default: none") do |generator|
+      options[:install] ||= []
+      options[:install] << generator
+    end
+    opts.on("-p", "--project=PROJECT", String,
+            "Rubyforge project name for the gem you are creating.",
+            "Default: same as gem name") { |x| options[:project] = x }
+    opts.on("-r", "--ruby=path", String,
+            "Path to the Ruby binary of your choice (otherwise scripts use env, dispatchers current path).",
+            "Default: #{DEFAULT_SHEBANG}") { |x| options[:shebang] = x }
+    opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+  end
+
+  def extract_options
+    @version           = options[:version].to_s.split(/\./)
+    @version_str       = @version.join('.')
+
+    @bin_names_list     = (options[:bin_name] || "").split(',')
+    @test_framework     = options[:test_framework] || "test_unit"
+    @project_name       = options[:project] if options.include?(:project)
+    @install_generators = options[:install] || []
+  end
+  
+  # first attempt to merge config args (single string) and runtime args
+  def config_args_and_runtime_args(runtime_args)
+    newgem_config = File.expand_path(File.join(ENV['HOME'], '.newgem.yml'))
+    if File.exists?(newgem_config)
+      config = YAML.load(File.read(newgem_config))
+      if config_args = (config["default"] || config[config.keys.first])
+        return config_args.split(" ") + runtime_args
+      end
+    end
+    runtime_args
+  end
+
+  # Installation skeleton.  Intermediate directories are automatically
+  # created so don't sweat their absence here.
+  BASEDIRS = %w(
+    cluster
+    cluster/actors
+    cluster/actions
+    cluster/triggers
+    cluster/resources
+    cluster/commands
+    config
+    lib
+    script
+    tasks
+    vendor
+  )
+end
