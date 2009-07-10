fc10cf02bcee6a736aac30fb51019c2b63726b6c
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-07-10 22:46:13 +0900

    Disabled to override Class.attr* methods. dump_attrs() dumps only attrs via def_attribute.

--- lib/wakame/packets.rb
+++ lib/wakame/packets.rb
@@ -10,7 +10,9 @@ module Wakame
     class ResponseBase
       include AttributeHelper
 
-      attr_reader :agent_id, :responded_at
+      #attr_reader :agent_id, :responded_at
+      def_attribute :agent_id
+      def_attribute :responded_at
       
       def initialize(agent)
         raise TypeError unless agent.respond_to?(:agent_id)
@@ -29,7 +31,9 @@ module Wakame
     class RequestBase
       include AttributeHelper
 
-      attr_reader :token, :requested_at
+      #attr_reader :token, :requested_at
+      def_attribute :token
+      def_attribute :requested_at
 
       def initialize(token=nil)
         @token = token || Util.gen_id
@@ -43,7 +47,12 @@ module Wakame
     end
     
     class Ping < ResponseBase
-      attr_reader :attrs, :monitors, :actors, :services
+      #attr_reader :attrs, :monitors, :actors, :services
+      def_attribute :attrs
+      def_attribute :monitors
+      def_attribute :actors
+      def_attribute :services
+
       def initialize(agent, attrs, actors, monitors, services)
         super(agent)
         @attrs = attrs
@@ -55,7 +64,10 @@ module Wakame
     
 
     class Register < ResponseBase
-      attr_reader :root_path, :attrs
+      #attr_reader :root_path, :attrs
+      def_attribute :root_path
+      def_attribute :attrs
+
       def initialize(agent, root_path, attrs)
         super(agent)
         @root_path = root_path
@@ -70,7 +82,9 @@ module Wakame
     end
 
     class MonitoringStarted < ResponseBase
-      attr_reader :svc_id
+      #attr_reader :svc_id
+      def_attribute :svc_id
+      
       def initialize(agent, svc_id)
         super(agent)
         @svc_id = svc_id
@@ -78,14 +92,19 @@ module Wakame
     end
 
     class MonitoringStopped < ResponseBase
-      attr_reader :svc_id
+      #attr_reader :svc_id
+      def_attribute :svc_id
+
       def initialize(agent, svc_id)
         super(agent)
         @svc_id = svc_id
       end
     end
     class MonitoringOutput < ResponseBase
-      attr_reader :svc_id, :outputs
+      #attr_reader :svc_id, :outputs
+      def_attribute :svc_id
+      def_attribute :outputs
+
       def initialize(agent, svc_id, outputs)
         super(agent)
         @svc_id = svc_id
@@ -94,7 +113,9 @@ module Wakame
     end
 
     class EventResponse < ResponseBase
-      attr_reader :event
+      #attr_reader :event
+      def_attribute :event
+
       def initialize(agent, event)
         super(agent)
         @event = event
@@ -127,7 +148,12 @@ module Wakame
 #     end
 
     class ServiceStatusChanged < ResponseBase
-      attr_accessor :svc_id, :prev_status, :new_status, :fail_message
+      #attr_accessor :svc_id, :prev_status, :new_status, :fail_message
+      def_attribute :svc_id
+      def_attribute :prev_status
+      def_attribute :new_status
+      def_attribute :fail_message
+
       def initialize(agent, svc_id, prev_status, new_status, fail_message=nil)
         super(agent)
         @svc_id = svc_id
@@ -138,7 +164,12 @@ module Wakame
     end
 
     class ActorRequest < RequestBase
-      attr_reader :agent_id, :token, :path, :args
+      #attr_reader :agent_id, :token, :path, :args
+      def_attribute :agent_id
+      def_attribute :token
+      def_attribute :path
+      def_attribute :args
+
       def initialize(agent_id, token, path, *args)
         super()
         @agent_id = agent_id
@@ -149,11 +180,17 @@ module Wakame
     end
 
     class ActorResponse < ResponseBase
-      attr_reader :agent_id, :token, :status
-      def initialize(agent, token, status)
+      #attr_reader :agent_id, :token, :status, :opts
+      def_attribute :agent_id
+      def_attribute :token
+      def_attribute :status
+      def_attribute :opts
+
+      def initialize(agent, token, status, opts={})
         super(agent)
         @token = token
         @status = status
+        @opts = opts
       end
     end
 
