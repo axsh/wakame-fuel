26cc9c340e442593a4376bc097f957684d13c977
Masahito Yoshida <masahito@axsh.net>
2009-07-13 17:06:17 +0900

    Add a method to execute sync command

--- lib/wakame/actor/system.rb
+++ lib/wakame/actor/system.rb
@@ -5,8 +5,23 @@ require 'ext/shellwords' unless Shellwords.respond_to? :shellescape
 
 class Wakame::Actor::System
   include Wakame::Actor
- 
+
+  def sync(count = 1)
+    count.to_i.times do |i|
+      Wakame.log.debug("Wakame::Actor::System.sync #{i + 1}/#{count}")
+      system("/bin/sync")
+    end
+  end
+
   def mount(dev, path, opts={})
+    16.times do |i|
+      break if File.blockdev?(dev)
+      Wakame.log.debug("Wakame::Actor::System.mount sync=#{i}")
+      self.sync
+      sleep 1.0
+    end
+
+    raise "#{dev} does not exist or not block device." unless File.blockdev?(dev)
     raise "#{path} does not exist or not directory." unless File.directory?(path)
     
     mount_point_dev=`/bin/df "#{path}" | /usr/bin/awk 'NR==2 {print $1}'`.strip
@@ -17,10 +32,10 @@ class Wakame::Actor::System
       Wakame.log.debug("Mounting volume: #{dev} as #{path} (with options: #{opts})")
       Wakame::Util.exec("/bin/mount -o #{mount_opts(opts)} '#{Shellwords.shellescape(dev)}' '#{Shellwords.shellescape(path)}'")
       # sync
-      3.times do |i|
-        system("/bin/sync")
-        sleep 1.0
-      end
+#      3.times do |i|
+#        system("/bin/sync")
+#        sleep 1.0
+#      end
     else
       Wakame.log.debug("Mounting EBS volume: #{dev} as #{path} (with options: #{opts})")
     end
