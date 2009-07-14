63de822c30076df04f45945363b8437638044cc2
Tsunehisa Iwano <t-iwano@axsh.net>
2009-07-14 19:17:22 +0900

    Add "stop_service" subcommand option to "-s"

--- lib/wakame/command/stop_service.rb
+++ lib/wakame/command/stop_service.rb
@@ -4,7 +4,19 @@ class Wakame::Command::StopService
   command_name='stop_service'
 
   def run(rule)
-      svc_inst = rule.service_cluster.instances
-      rule.trigger_action(Wakame::Actions::StopService.new(svc_inst[@options["instances"]]))
+      unless @options["instances"].nil?
+        svc_inst = rule.service_cluster.instances
+        rule.trigger_action(Wakame::Actions::StopService.new(svc_inst[@options["instances"]]))
+      end
+      levels = rule.service_cluster.dg.levels
+      levels.reverse.each { |lv|
+        lv.each { |svc_prop|
+	if svc_prop.class.to_s == @options["service"].to_s
+          rule.service_cluster.each_instance(svc_prop.class) { |svc_inst|
+            rule.trigger_action(Wakame::Actions::StopService.new(svc_inst))
+          }
+	end
+        }
+      }
   end
 end
