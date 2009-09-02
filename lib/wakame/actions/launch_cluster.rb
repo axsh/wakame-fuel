e1dd92d21231466a25ca8851db96ca7fc294311a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-09-02 21:06:56 +0900

    Update LaunchCluster action to trigger StartService instead of PropagateInstances.
    Previously, the cluster model was designed to create service instance objects during launch time. The design has been changed
    to just kick services off by retrieving the existing object tree.

--- lib/wakame/actions/launch_cluster.rb
+++ lib/wakame/actions/launch_cluster.rb
@@ -11,17 +11,19 @@ module Wakame
         end
 
         levels = service_cluster.dg.levels
-        Wakame.log.debug("#{self.class}: Resource launch order: " + levels.collect {|lv| '['+ lv.collect{|prop| "#{prop.class}" }.join(', ') + ']' }.join(', '))
+        Wakame.log.debug("#{self.class}: Scheduled resource launch order: " + levels.collect {|lv| '['+ lv.collect{|prop| "#{prop.class}" }.join(', ') + ']' }.join(', '))
         acquire_lock { |list|
           levels.each {|lv| list << lv.collect{|res| res.class } }
         }
 
         levels.each { |lv|
-          lv.each { |svc_prop|
-            trigger_action(PropagateInstances.new(svc_prop))
+          Wakame.log.info("#{self.class}: Launching resources: #{lv.collect{|prop| "#{prop.class}" }.join(', ')}")
+          lv.each { |resource|
+            cluster.each_instance(resource) { |svc|
+              trigger_action(StartService.new(svc))
+            }
           }
           flush_subactions
-          Wakame.log.debug("#{self.class}: DG level next")
         }
       end
 
