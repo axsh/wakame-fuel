52bf8f4c7c89ed87c2a26e32f47befd4c4f83639
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-09-03 22:28:51 +0900

    Change the second argument to accept Service::VmSpec object.
    Service::Cluster is changed to have VM template value which will be used to launch new VM.
    So that LaunchVM action has been updated to use and merge that value at launching VM.

--- lib/wakame/actions/launch_vm.rb
+++ lib/wakame/actions/launch_vm.rb
@@ -1,45 +1,40 @@
 module Wakame
   module Actions
     class LaunchVM < Action
-      def initialize(notes_key, ref_agent=nil)
-        @ref_agent = ref_agent
+      def initialize(notes_key, vm_spec)
         @notes_key = notes_key
+        @vm_spec = vm_spec
       end
 
       USER_DATA_TMPL=<<__END__
 node=agent
-amqp_server=amqp://%s/
-agent_id=%s
+amqp_server=%s
 __END__
 
       def run
         require 'right_aws'
         ec2 = RightAws::Ec2.new(Wakame.config.aws_access_key, Wakame.config.aws_secret_key, {:cache=>false})
 
-        # Fetch the reference vm slice attributes.
-        @ref_agent ||= master.agent_monitor.master_local
-        ref_attr = ec2.describe_instances([@ref_agent.attr[:instance_id]])
-        ref_attr = ref_attr[0]
-        
+        ref_attr = @vm_spec.merge(cluster.template_vm_attr)
 
-        user_data = sprintf(USER_DATA_TMPL, master.attr[:local_ipv4], '')
+        user_data = sprintf(USER_DATA_TMPL, cluster.advertised_amqp_servers)
         Wakame.log.debug("#{self.class}: Lauching VM: #{ref_attr.inspect}\nuser_data: #{user_data}")
-        res = ec2.run_instances(ref_attr[:aws_image_id], 1, 1,
-                                ref_attr[:aws_groups],
-                                ref_attr[:ssh_key_name],
+        res = ec2.run_instances(ref_attr[:image_id], 1, 1,
+                                ref_attr[:security_groups],
+                                ref_attr[:key_name],
                                 user_data,
                                 'public', # addressing_type
-                                ref_attr[:aws_instance_type], # instance_type
+                                ref_attr[:instance_type], # instance_type
                                 nil, # kernel_id
                                 nil, # ramdisk_id
-                                ref_attr[:aws_availability_zone], # availability_zone
+                                ref_attr[:availability_zone], # availability_zone
                                 nil # block_device_mappings
                                 )[0]
         inst_id = res[:aws_instance_id]
 
         ConditionalWait.wait { | cond |
           cond.wait_event(Event::AgentMonitored) { |event|
-            event.agent.attr[:instance_id] == inst_id
+            event.agent.vm_attr[:instance_id] == inst_id
           }
           
           cond.poll(5, 360) {
