559f2891e3a70815085a22534dd9dfb2e9f92f5e
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-09-06 22:18:49 +0900

    Update to recognize new propergate_service command options.

--- lib/wakame/command/propagate_service.rb
+++ lib/wakame/command/propagate_service.rb
@@ -1,32 +1,29 @@
 
 class Wakame::Command::PropagateService
   include Wakame::Command
+  include Wakame
 
-  #command_name='launch_cluster'
+  command_name='propagate_service'
 
-  def parse(args)
-    @resource
-  end
+  def run(trigger)
+    refsvc = Service::ServiceInstance.find(@options["service_id"]) || raise("Unknown ServiceInstance: #{@options["service_id"]}")
 
-  def run(rule)
-    prop = nil
-    prop = rule.service_cluster.properties[@options["service"]]
-    if prop.nil?
-      raise "UnknownProperty: #{@options["service"]}" 
+    host_id = @options["host_id"]
+    unless host_id.nil?
+      host = Service::Host.find(host_id) || raise("Specified host was not found: #{host_id}")
+      raise "Same resouce type is already assigned: #{refsvc.resource.class} on #{host_id}" if host.has_resource_type?(refsvc.resource)
     end
 
-    @num = nil
-    @num = @options["num"] || 1
+    num = @options["number"] || 1
+    raise "Invalid format of number: #{num}" unless /^(\d+)$/ =~ num.to_s
+    num = num.to_i
 
-    unless /^(\d){1,32}$/ =~ @num.to_s
-      raise "The number is not appropriate: #{@num}"
+    if num < 1 || refsvc.resource.max_instances < trigger.cluster.instance_count(refsvc.resource) + num
+      raise "The number must be between 1 and #{refsvc.resource.max_instances - trigger.cluster.instance_count(refsvc.resource)} (max limit: #{refsvc.resource.max_instances})"
     end
 
-    instance = rule.master.service_cluster.dump_status[:properties][@options["service"]]
-    if @num.to_i > (instance[:max_instances].to_i - instance[:instances].count.to_i)
-      raise "The number is not appropriate: #{@num}"
-    end
-
-    rule.trigger_action(Wakame::Actions::PropagateInstances.new(prop, @num.to_i))
+    num.times {
+      trigger.trigger_action(Wakame::Actions::PropagateService.new(refsvc))
+    }
   end
 end
