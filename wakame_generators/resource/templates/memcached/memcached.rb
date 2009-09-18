569a0f2f3a203271edb1aec9ab80187683a2f315
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-09-18 19:43:43 +0900

    Update resource template files under wakame_generators folder.

--- wakame_generators/resource/templates/memcached/memcached.rb
+++ wakame_generators/resource/templates/memcached/memcached.rb
@@ -1,37 +1,33 @@
-require 'wakame/rule'
-
 class Memcached < Wakame::Service::Resource
 
-  def_attribute :duplicable, true
-  def_attribute :max_instances, {:default=>5}
-  
-  def_attribute :listen_port,  {:default => 11211 }
-  def_attribute :bind_address, {:default => '127.0.0.1'}
-  def_attribute :memory_size,  {:default => 64}
-  def_attribute :user,         {:default => 'nobody'}
+  property :listen_port,  {:default => 11211 }
+  property :bind_address, {:default => '127.0.0.1'}
+  property :memory_size,  {:default => 64}
+  property :user,         {:default => 'nobody'}
 
   def render_config(template)
-    template.cp(%w(init.d/memcached))
-    template.render(%w(conf/memcached.conf))
+    template.cp('init.d/memcached')
+    template.render('conf/memcached.conf')
     template.chmod("init.d/memcached", 0755)
   end
 
   def on_parent_changed(svc, action)
-    Wakame::Rule::BasicActionSet.deploy_configuration(svc)
+    action.trigger_action(Wakame::Actions::DeployConfig.new(svc))
+    action.flush_subactions
     reload(svc, action)
   end
 
   def start(svc, action)
     cond = ConditionalWait.new { |cond|
       cond.wait_event(Wakame::Event::ServiceOnline) { |event|
-        event.instance_id == svc.instance_id
+        event.instance_id == svc.id
       }
     }
 
-    request = action.actor_request(svc.agent.agent_id,
+    request = action.actor_request(svc.cloud_host.agent_id,
                                    '/service_monitor/register',
-                                   svc.instance_id, :pidfile, '/var/run/memcached.pid').request
-    action.actor_request(svc.agent.agent_id,
+                                   svc.id, :pidfile, '/var/run/memcached.pid').request
+    action.actor_request(svc.cloud_host.agent_id,
                          '/daemon/start', "memcached", 'init.d/memcached'){ |req|
       req.wait
       Wakame.log.debug("#{self.class} process started")
@@ -43,11 +39,11 @@ class Memcached < Wakame::Service::Resource
   def stop(svc, action)
     cond = ConditionalWait.new { |cond|
       cond.wait_event(Wakame::Event::ServiceOffline) { |event|
-        event.instance_id == svc.instance_id
+        event.instance_id == svc.id
       }
     }
 
-    action.actor_request(svc.agent.agent_id,
+    action.actor_request(svc.cloud_host.agent_id,
                          '/daemon/stop', 'memcached', 'init.d/memcached'){ |req|
       req.wait
       Wakame.log.debug("#{self.class} process stopped")
@@ -55,18 +51,18 @@ class Memcached < Wakame::Service::Resource
 
     cond.wait
 
-    request = action.actor_request(svc.agent.agent_id,
+    request = action.actor_request(svc.cloud_host.agent_id,
                                    '/service_monitor/unregister', svc.instance_id).request
   end
 
   def reload(svc, action)
-    action.actor_request(svc.agent.agent_id,
+    action.actor_request(svc.cloud_host.agent_id,
                          '/daemon/stop', 'memcached', 'init.d/memcached'){ |req|
       req.wait
       # Wakame.log.debug("#{self.class} process stopped")
     }
 
-    action.actor_request(svc.agent.agent_id,
+    action.actor_request(svc.cloud_host.agent_id,
                          '/daemon/start', "memcached", 'init.d/memcached'){ |req|
       req.wait
       # Wakame.log.debug("#{self.class} process started")
--- wakame_generators/resource/templates/mysql_slave/mysql_slave.rb
+++ wakame_generators/resource/templates/mysql_slave/mysql_slave.rb
@@ -1,14 +1,11 @@
 
 class MySQL_Slave < Wakame::Service::Resource
 
-  def_attribute :duplicable, true
-  def_attribute :max_instances, {:default=>5}
+  property :mysqld_basedir, {:default=>'/home/wakame/mysql'}
+  property :mysqld_port, {:default=>3307}
 
-  def_attribute :mysqld_basedir, '/home/wakame/mysql'
-  def_attribute :mysqld_port, 3307
-
-  def_attribute :ebs_device, '/dev/sdn'
-  def_attribute :ebs_mount_option, 'noatime'
+  property :ebs_device
+  property :ebs_mount_option, {:default=>'noatime'}
 
   def basedir
     File.join(Wakame.config.root_path, 'cluster', 'resources', 'mysql_slave')
@@ -23,22 +20,22 @@ class MySQL_Slave < Wakame::Service::Resource
   end
 
   def render_config(template)
-    template.cp(%w(init.d/mysql-slave))
-    template.render(%w(conf/my.cnf))
-    template.chmod("init.d/mysql-slave", 0755)
+    template.cp('init.d/mysql-slave')
+    template.render('conf/my.cnf')
+    template.chmod('init.d/mysql-slave', 0755)
   end
 
   def start(svc, action)
     cond = ConditionalWait.new { |cond|
       cond.wait_event(Wakame::Event::ServiceOnline) { |event|
-        event.instance_id == svc.instance_id
+        event.instance_id == svc.id
       }
     }
 
-    action.actor_request(svc.agent.agent_id,
+    action.actor_request(svc.cloud_host.agent_id,
                          '/service_monitor/register',
-                         svc.instance_id,
-                         :command, "/usr/bin/mysqladmin --defaults-file=#{svc.agent.root_path}/tmp/config/mysql_slave/conf/my.cnf ping > /dev/null") { |req|
+                         svc.id,
+                         :command, "/usr/bin/mysqladmin --defaults-file=#{svc.cloud_host.root_path}/tmp/config/mysql_slave/conf/my.cnf ping > /dev/null") { |req|
     }
 
     opt_map = {
@@ -55,22 +52,22 @@ class MySQL_Slave < Wakame::Service::Resource
       opt_map[:repl_pass]             = mysql_master.resource.repl_pass
     }
 
-    action.actor_request(svc.agent.agent_id, '/mysql/take_master_snapshot', opt_map) { |req|
+    action.actor_request(svc.cloud_host.agent_id, '/mysql/take_master_snapshot', opt_map) { |req|
       req.wait
       Wakame.log.debug("take-master-snapshot!!")
     }
 
-    action.actor_request(svc.agent.agent_id, '/system/sync') { |req|
+    action.actor_request(svc.cloud_host.agent_id, '/system/sync') { |req|
       req.wait
       Wakame.log.debug("sync")
     }
 
-    action.actor_request(svc.agent.agent_id, '/system/mount', self.ebs_device, self.mysqld_datadir, self.ebs_mount_option) { |req|
+    action.actor_request(svc.cloud_host.agent_id, '/system/mount', self.ebs_device, self.mysqld_datadir, self.ebs_mount_option) { |req|
       req.wait
       Wakame.log.debug("MySQL volume was mounted: #{self.mysqld_datadir}")
     }
 
-    action.actor_request(svc.agent.agent_id, '/daemon/start', 'mysql_slave', 'init.d/mysql-slave') { |req|
+    action.actor_request(svc.cloud_host.agent_id, '/daemon/start', 'mysql_slave', 'init.d/mysql-slave') { |req|
       req.wait
       Wakame.log.debug("MySQL process started")
     }
@@ -80,11 +77,11 @@ class MySQL_Slave < Wakame::Service::Resource
   def stop(svc, action)
     cond = ConditionalWait.new { |c|
       c.wait_event(Wakame::Event::ServiceOffline) { |event|
-        event.instance_id == svc.instance_id
+        event.instance_id == svc.id
       }
     }
-    action.actor_request(svc.agent.agent_id, '/daemon/stop', 'mysql_slave', 'init.d/mysql-slave') { |req| req.wait }
-    action.actor_request(svc.agent.agent_id, '/system/umount', self.mysqld_datadir) { |req|
+    action.actor_request(svc.cloud_host.agent_id, '/daemon/stop', 'mysql_slave', 'init.d/mysql-slave') { |req| req.wait }
+    action.actor_request(svc.cloud_host.agent_id, '/system/umount', self.mysqld_datadir) { |req|
       req.wait
       Wakame.log.debug("MySQL volume unmounted")
     }
@@ -93,7 +90,7 @@ class MySQL_Slave < Wakame::Service::Resource
     require 'right_aws'
     ec2 = RightAws::Ec2.new(Wakame.config.aws_access_key, Wakame.config.aws_secret_key)
     ec2.describe_volumes.each do |volume|
-      next unless volume[:aws_instance_id] == svc.agent.agent_id && volume[:aws_device] == self.ebs_device
+      next unless volume[:aws_instance_id] == svc.cloud_host.agent_id && volume[:aws_device] == self.ebs_device
 
       @ebs_volume = volume[:aws_id]
 
@@ -115,9 +112,9 @@ class MySQL_Slave < Wakame::Service::Resource
     end
 
     # unregister
-    action.actor_request(svc.agent.agent_id,
+    action.actor_request(svc.cloud_host.agent_id,
                          '/service_monitor/unregister',
-                         svc.instance_id).request
+                         svc.id).request
 
   end
 end
