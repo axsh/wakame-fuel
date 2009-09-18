3b5e87e461728a6c76bdcb7d045953ff57f84b49
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-09-18 22:11:59 +0900

    Reordered loading data in command/status.rb.

--- lib/wakame/command/status.rb
+++ lib/wakame/command/status.rb
@@ -13,30 +13,28 @@ class Wakame::Command::Status
         :resources=>{},
         :cloud_hosts=>{}
       }
-      cluster_id = master.cluster_manager.clusters.keys.first
-      if cluster_id.nil?
-        raise "There is no cluster setting"
-      end
 
-      cluster = ServiceCluster.find(cluster_id)
-      res[:cluster] = cluster.dump_attrs
       res[:agent_pool] = AgentPool.instance.dump_attrs
-
       AgentPool.instance.group_active.keys.each { |id|
         res[:agents][id] = Agent.find(id).dump_attrs
       }
 
-      cluster.services.keys.each { |id|
-        res[:services][id]=ServiceInstance.find(id).dump_attrs
-      }
-
-      cluster.resources.keys.each { |id|
-        res[:resources][id]=Resource.find(id).dump_attrs
-      }
-
-      cluster.cloud_hosts.keys.each { |id|
-        res[:cloud_hosts][id]=CloudHost.find(id).dump_attrs
-      }
+      cluster_id = master.cluster_manager.clusters.first
+      if cluster_id
+        cluster = ServiceCluster.find(cluster_id)
+        res[:cluster] = cluster.dump_attrs
+        cluster.services.keys.each { |id|
+          res[:services][id]=ServiceInstance.find(id).dump_attrs
+        }
+        
+        cluster.resources.keys.each { |id|
+          res[:resources][id]=Resource.find(id).dump_attrs
+        }
+        
+        cluster.cloud_hosts.keys.each { |id|
+          res[:cloud_hosts][id]=CloudHost.find(id).dump_attrs
+        }
+      end
 #p res[:cluster]
       res
     }
