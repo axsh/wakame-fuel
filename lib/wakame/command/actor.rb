bcae8b59d7f4e5f7be2617d4e70927991611fdb2
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-10-05 23:36:16 +0900

    Update server command "actor".

--- lib/wakame/command/actor.rb
+++ lib/wakame/command/actor.rb
@@ -5,19 +5,16 @@ class Wakame::Command::Actor
 
   command_name='actor'
 
-  def parse(args)
-    raise "Not enugh number of arguments" if args.size < 2
-  end
-
   def run
-    agent = Agent.find(params[:agent_id])
-    raise "Unknown agent: #{params[:agent_id]}" if agent.nil?
-    raise "Invalid agent status (Not Online): #{agent.status} #{params[:agent_id]}" if agent.status != Agent::STATUS_ONLINE
+    agent = Agent.find(params['agent_id'])
+    raise "Unknown agent: #{params['agent_id']}" if agent.nil?
+    raise "Invalid agent status (Not Online): #{agent.status} #{params['agent_id']}" if agent.status != Agent::STATUS_ONLINE
+    raise "Invalid actor path: #{params['path']}" if params['path'].nil? || params['path'] == ''
+    if params['args'].is_a? String
+      params['args'] = eval(params['args'])
+    end
 
-    raise "Invalid actor path: #{params[:path]}" if params[:path].nil? || params[:path] == ''
-
-    request = rule.master.actor_request(params[:agent_id], params[:path], *params[:args]).request
-    request.wait
+    request = master.actor_request(params['agent_id'], params['path'], *params['args']).request
   end
 
 end
