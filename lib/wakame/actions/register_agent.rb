10693f425c4f68383cc2e721a3955a824a66328f
Masahiro Fujiwara <m-fujiwara@axsh.net>
2009-10-16 21:10:08 +0900

    Add debug log.

--- lib/wakame/actions/register_agent.rb
+++ lib/wakame/actions/register_agent.rb
@@ -14,6 +14,7 @@ module Wakame
         
         # Send monitoring conf
         if @agent.cloud_host_id
+          Wakame.log.debug("#{self.class}: #{@agent.id} is to be set the monitor conf: #{@agent.cloud_host.monitors.inspect}")
           @agent.cloud_host.monitors.each { |path, data|
             master.actor_request(@agent.id, '/monitor/reload', path, data).request.wait
           }
